bb7773377dcba93bd5ac321c39b8e9253741997b
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-09 00:02:06 -0400

    Added test cases for calculateBottles

--- test/bottlesTest.js
+++ test/bottlesTest.js
@@ -1,5 +1,15 @@
 const chai = require("chai").assert;
+const { assert } = require("chai");
+const { calculateBottles } = require("../bottles");
 
-describe("generateBreakdown", () => {
-  
+describe("calculateBottles", () => {
+  it("should return 55 with input 30", () => {
+    const actual = calculateBottles(30);
+    assert.equal(actual, 55);
+  });
+
+  it("should return 0 with input 0", () => {
+    const actual = calculateBottles(0);
+    assert.equal(actual, 0);
+  });
 });
